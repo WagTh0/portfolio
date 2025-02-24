5270ac990d43f767b3c77b55ca2eaa9713fed0c2
WagTh0 <ranger235711@gmail.com>
2025-02-24 14:08:10 -0800

    Add files via upload

---
+++ fanartwork.js
@@ -0,0 +1,25 @@
+function renderArtwork(galleryId, artworkArray) {
+    const gallery = document.getElementById(galleryId);
+    artworkArray.forEach(art => {
+        const artDiv = document.createElement("div");
+        artDiv.className = "art_piece";
+        artDiv.innerHTML = `
+            <img 
+                src="${art.src}" 
+                alt="${art.alt}" 
+                data-title="${art.title}" 
+                data-info="${art.info}" 
+                data-bts="${art.bts}"
+                onclick="showPopup(this)"
+            >
+        `;
+        gallery.appendChild(artDiv);
+    });
+}
+
+fetch('artworks.json')
+    .then(response => response.json())
+    .then(data => {
+        renderArtwork("fan-art-gallery", data.fanArt);
+    })
+    .catch(error => console.error('Error loading artwork data:', error));
